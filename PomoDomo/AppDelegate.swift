f9b231bb3e45d7f2ed3990da3515105bf439faac
Danielle Golinsky <degolinsky@icloud.com>
2018-12-27 11:53:13 -0500

    Initial Commit

---
+++ PomoDomo/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  PomoDomo
+//
+//  Created by Danielle Golinsky on 12/27/18.
+//  Copyright © 2018 Danielle Golinsky. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ PomoDomo/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  PomoDomo
+//
+//  Created by Danielle Golinsky on 12/27/18.
+//  Copyright © 2018 Danielle Golinsky. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
