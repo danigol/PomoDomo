b8c3ef65b57610476d1ed04b49be8694ec743796
Danielle Golinsky <degolinsky@icloud.com>
2018-12-27 17:51:37 -0500

    Updated to just be a set and forget app.

--- PomoDomo/AppDelegate.swift
+++ PomoDomo/AppDelegate.swift
@@ -13,12 +13,13 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     @IBOutlet weak var menu: NSMenu!
     public var interval: Int32 = 20
-    public var timeLeft: Int32!
+    public var timeLeft: Int32 = 0
     
     @IBOutlet weak var startTimerMenuItem: NSMenuItem!
     @IBOutlet weak var stopTimerMenuItem: NSMenuItem!
     @IBOutlet weak var resetTimerMenuItem: NSMenuItem!
     
+    let notification = NSUserNotification()
     var statusItem: NSStatusItem?
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
@@ -37,12 +38,19 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     }
 
     @IBAction func startTimer(_ sender: Any) {
-        startTimerMenuItem.isHidden = true
-        stopTimerMenuItem.isHidden = false
-        resetTimerMenuItem.isHidden = false
+        // TODO Temporarily just making it set and forget. But will have to return to this
+//        startTimerMenuItem.isHidden = true
+//        stopTimerMenuItem.isHidden = false
+//        resetTimerMenuItem.isHidden = false
         
-        // TODO Start the bloody timer
-        timeLeft = interval
+        if timeLeft == 0 {
+            timeLeft = interval
+        }
+        
+        // TODO Start an actual timer.
+        showTimesUpNotification(title: "Timer Finished!",
+                                subTitle: "Time to take a break!",
+                                deliveryDate: Date(timeIntervalSinceNow: TimeInterval(timeLeft * 60)))
     }
     
     @IBAction func stopTimer(_ sender: Any) {
@@ -50,7 +58,10 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         stopTimerMenuItem.isHidden = true
         resetTimerMenuItem.isHidden = true
         
-        // TODO Stops the timer
+        let intervalLeft = notification.deliveryDate?.timeIntervalSinceNow
+        timeLeft = Int32(intervalLeft!)
+        
+        NSUserNotificationCenter.cancelPreviousPerformRequests(withTarget: notification)
     }
     
     @IBAction func resetTimer(_ sender: Any) {
@@ -62,7 +73,14 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         timeLeft = interval
     }
     
-    
+    func showTimesUpNotification(title: String, subTitle: String, deliveryDate: Date) {
+        notification.title = title
+        notification.subtitle = subTitle
+        notification.soundName = "Purr"
+        
+        notification.deliveryDate = deliveryDate
+        NSUserNotificationCenter.default.scheduleNotification(notification)
+    }
     
 }
 
