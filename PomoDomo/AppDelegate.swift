b77baa4b80aa2f4adcbecba980f62f6b1e7b6489
Danielle Golinsky <degolinsky@icloud.com>
2018-12-27 15:44:13 -0500

    Simplified and added menu buttons to start/stop timer.

--- PomoDomo/AppDelegate.swift
+++ PomoDomo/AppDelegate.swift
@@ -13,6 +13,11 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     @IBOutlet weak var menu: NSMenu!
     public var interval: Int32 = 20
+    public var timeLeft: Int32!
+    
+    @IBOutlet weak var startTimerMenuItem: NSMenuItem!
+    @IBOutlet weak var stopTimerMenuItem: NSMenuItem!
+    @IBOutlet weak var resetTimerMenuItem: NSMenuItem!
     
     var statusItem: NSStatusItem?
 
@@ -21,12 +26,43 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         self.statusItem?.button?.title = "PomoDomo!"
         self.statusItem?.button?.image = NSImage(named:NSImage.Name("PomoDomo_ic64.png"))
         self.statusItem!.menu = self.menu
+        
+        startTimerMenuItem.isHidden = false
+        stopTimerMenuItem.isHidden = true
+        resetTimerMenuItem.isHidden = true
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
 
-
+    @IBAction func startTimer(_ sender: Any) {
+        startTimerMenuItem.isHidden = true
+        stopTimerMenuItem.isHidden = false
+        resetTimerMenuItem.isHidden = false
+        
+        // TODO Start the bloody timer
+        timeLeft = interval
+    }
+    
+    @IBAction func stopTimer(_ sender: Any) {
+        startTimerMenuItem.isHidden = false
+        stopTimerMenuItem.isHidden = true
+        resetTimerMenuItem.isHidden = true
+        
+        // TODO Stops the timer
+    }
+    
+    @IBAction func resetTimer(_ sender: Any) {
+        startTimerMenuItem.isHidden = false
+        stopTimerMenuItem.isHidden = true
+        resetTimerMenuItem.isHidden = true
+        
+        // TODO Stops the timer & Resets
+        timeLeft = interval
+    }
+    
+    
+    
 }
 
